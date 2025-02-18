a8be1a2b5eafd8e5c4dad74b6b28ffa65efd72f1
Ray <whdev36@gmail.com>
2025-02-17 19:41:44 -0800

    feat: member model and view
    Member model and view function created

--- members/models.py
+++ members/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Member(AbstractUser):
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.username
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
--- members/views.py
+++ members/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    all_members = Member.objects.all()
+    template = loader.get_template('all_members.html')
+    return HttpResponse(template.render({'members': all_members.values()}, request))
